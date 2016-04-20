400aadcac7bfbb0aba7faf4186f3c3bafb59e89f
glennc <glennc@microsoft.com>
2016-04-20 19:14:24 -0400

    0.0.2

--- dotnet-packages.rb
+++ dotnet-packages.rb
@@ -1,14 +1,14 @@
 class DotnetPackages < Formula
   desc "Command line tool to examine the dependencies of your .NET application"
   homepage "https://github.com/glennc/dotnet-packages"
-  url "https://github.com/glennc/dotnet-packages/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "a2ad5c9bccd9d7c42ae99434b7852b28a19774e2515e5eeb595ec8cc2d1cc849"
+  url "https://github.com/glennc/dotnet-packages/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "ebb151dd8de486494d337e427fff31fa64276184dad447886997313633679f25"
 
   bottle do
     cellar :any
-    root_url "https://github.com/glennc/dotnet-packages/releases/download/0.0.1"
-    sha256 "ff6584964fff27489c8492b29857d07cc0c43f25dfafa5c88de5d47ea48b7350" => :el_capitan
+    root_url "https://github.com/glennc/dotnet-packages/releases/download/0.0.2"
+    sha256 "8e35ef85c16f98fdeb988a6a984257837893897e81b79d5c35573871fe5fca57" => :el_capitan    
   end
 
   def install
@@ -16,6 +16,6 @@ class DotnetPackages < Formula
     system "/usr/local/bin/dotnet", "publish", "-o", "./output", "--configuration", "release"
     prefix.install Dir["output/*"]
 
-    bin.install_symlink "#{prefix}/dotnet-packages-0.0.1" => "dotnet-packages"
+    bin.install_symlink "#{prefix}/dotnet-packages-0.0.2" => "dotnet-packages"
   end
 end
